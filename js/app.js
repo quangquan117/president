3dc108e9d413698357a0ebcdd28a1cce1ad713ba
quangquan <leo.langlois2@gmail.com>
2023-12-19 10:31:02 +0100

    [add] all

---
+++ js/app.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", e => {
+    console.dir("here");
+
+    let xml, xmlhttp, xmlDoc, vliste;
+
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "api/president.xml", false);
+    xmlhttp.send();
+    xmlDoc = xmlhttp.responseXML;
+    console.log(xmlDoc);
+    vliste = String("");
+    xml = xmlDoc.getElementsByTagName("president");
+
+    console.log(xml[0].childNodes[1]);
+    for (let i = 0; i < xml.length; i++) {
+
+        //variables pour les noeuds
+
+        const Nom = xml[i].getElementsByTagName("Nom")[0].childNodes[0].nodeValue;
+        const Mandat = xml[i].getElementsByTagName("Mandat")[0].childNodes[0].nodeValue;
+        const Naissance = xml[i].getElementsByTagName("Naissance")[0].childNodes[0].nodeValue;
+        const Mort = xml[i].getElementsByTagName("Mort")[0].childNodes[0].nodeValue;
+        const Image = xml[i].getElementsByTagName("Image")[0].getAttribute("path");
+
+        //ajout de chaque propriété dans boucle
+        console.log(Nom + " " + Mandat + " " + Naissance + " " + Mort);
+
+        vliste += "<li>Nom : " + Nom + "</li>";
+        vliste += "<li>Mandat : " + Mandat + "</li>";
+        vliste += "<li>Naissance : " + Naissance + "</li>";
+        vliste += "<li>Mort : " + Mort + "</li>";
+        vliste += `<img src="${Image}" alt="${Nom}"></img>`;
+        //stockage dans un tableau
+        myStock = [];
+        myStock.push(Nom, Mandat, Naissance, Mort, Image);
+        console.log(JSON.stringify(myStock));
+        localStorage.setItem("stock", myStock);
+    }
+
+    document.querySelector("ul").innerHTML = vliste;
+});
